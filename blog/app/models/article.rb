c833ef0ad448d1b9bbc735d9c6ca9b6605830a6b
karen0047 <karen0047@gmail.com>
2017-02-02 11:01:07 +0300

    Before_destroy

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,18 @@
 class Article < ApplicationRecord
-	validates :title, presence: true, length: { minimum: 3 }
+	
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :text, presence: true, length: { minimum: 1 }
+  
   scope :published, -> { where(published: true) }
-  validates :text, presence: true, length: { minimum: 200 }
+  
+  before_destroy :control
+
+  def control
+    if Article.published
+      errors.add :base, "Невозможно удалить"
+      return false
+    else
+      true
+    end
+  end
 end  
